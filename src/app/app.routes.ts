e61afeb30e3985a7ddaa3f81ee5a1094321c8cae
unknown <faqquu>
2025-04-09 10:52:01 -0300

    creacion de contacto, con navbar y footer

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import path from 'node:path';
+import { InicioComponent } from './inicio/inicio/inicio.component';
+import { ContactoComponent } from './contacto/contacto/contacto.component';
+
+export const routes: Routes = [
+    {path: '', redirectTo : '/inicio', pathMatch: 'full'},
+    {path: 'inicio', component:InicioComponent},
+    {path: '', redirectTo : '/contacto', pathMatch: 'full'},
+    {path: 'contacto', component:ContactoComponent}
+];
